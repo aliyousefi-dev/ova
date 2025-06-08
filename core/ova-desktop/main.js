2493a48bef8b53c926e2d67e6726b851e8e0e6f4
ali <aliyousefiorg@gmail.com>
2025-06-08 03:44:37 +0330

    adding Tailwind and DaisyUI to Desktop

--- core/ova-desktop/main.js
+++ core/ova-desktop/main.js
@@ -6,22 +6,19 @@ process.on('uncaughtException', (error) => {
     console.error("Unexpected error: ", error);
 });
 function createWindow() {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
-            contextIsolation: true,
-            enableRemoteModule: false,
-        }
-    });
-    win.loadURL('http://localhost:4200');
+    win = new BrowserWindow({width: 800, height: 800});
+    win.loadFile('dist/ova-desktop/browser/index.html');
 }
+
 // App Lifecycle
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+    createWindow()
+})
+
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit();
 });
+
 app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
