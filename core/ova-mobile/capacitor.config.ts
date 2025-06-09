f751a5bdf723b8d0493584c43a2535de9c675b1a
ali <aliyousefiorg@gmail.com>
2025-06-09 04:44:04 +0330

    adding hte Android Project

---
+++ core/ova-mobile/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ova.mobile',
+  appName: 'ova mobile',
+  webDir: 'dist/ova-mobile/browser',
+};
+
+export default config;
